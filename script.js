30a8c8b3b54d4ae08fb96d0af3d6bec1393cd64b
yyx <yuan_xin96@hotmail.com>
2018-03-05 20:26:55 +0800

    adding my work

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-tasks = ["buy milk", "eat dinner", "nail javascript"]
+var tasks = ["buy milk", "eat dinner", "nail javascript"]
+
+// display existing task list
+for (let i=0; i<tasks.length; i++){
+    addNewItem(tasks[i]);
+}
+
+function addNewItem (todoContent) {
+    var createElement = document.createElement("li"); 
+    createElement.textContent = todoContent;
+    createElement.addEventListener("click", markItDown);  
+    document.getElementById("todo-list").appendChild(createElement); 
+}
+
+function markItDown(event){
+    if (event.target.classList.contains('done')){
+        event.target.classList.remove('done'); 
+    } else {
+        event.target.classList.add('done'); 
+    }
+}
+
+var formItem = document.getElementsByTagName('form')[0]; 
+formItem.addEventListener('submit',function (event) {
+    event.preventDefault();
+    var Content = document.getElementById('add-todo-list').value;
+    if( Content.length !== 0 ){
+        tasks.push(Content);
+        addNewItem(Content)
+        document.getElementById('add-todo-list').value =""
+    } else {
+        alert("Invalid Input"); 
+    }
+}); 
+
+/*
+// note: completedItems is an array not an element
+var completedItems = document.getElementsByTagName('li');
+for (let i = 0; i < completedItems.length; i++) {
+    completedItems[i].addEventListener('click', function() {
+        if (completedItems[i].classList.contains('done')){
+            completedItems[i].classList.remove('done');
+        } else {
+            completedItems[i].classList.add('done');
+        }
+    });
+}
+*/ 
